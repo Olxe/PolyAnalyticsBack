6e448458d070251cd39c2fd7986538c34109e9ed
Bugnone <bugnone.michael@gmail.com>
2020-09-10 17:15:07 +0200

    follower

--- src/tweet.js
+++ src/tweet.js
@@ -1,23 +1,44 @@
 var TweetSchema = require('../schema/tweetSchema.js');
 
+var Twit = require('twit');
+var config = require('./config.js');
+var T = new Twit(config);
+
 module.exports = {
   StartTweetStream: async function() {
     console.log('Twitter streaming is starting');
 
-    var Twit = require('twit');
-    var config = require('./config.js');
-    
-    var T = new Twit(config);
-    var stream = T.stream('statuses/sample')
+    T.get('followers/ids', { screen_name: 'MichaelBugnone' },  function (err, data, response) {
+      for (var i = 0; i < data['ids'].length; i++) {
+        // console.log(data['ids'][i]);
 
-    stream.on('tweet', function (tweet) {
-      onTweetReceived(tweet);
+        if(data.length > 0) {
+          var id = data[0]['ids']
+          console.log(id);
+        }
+        // getFollowerLookup(data['ids'][i]);
+      }
     });
+
+    // var stream = T.stream('statuses/sample')
+
+    // stream.on('tweet', function (tweet) {
+    //   console.log(tweet);
+    //   // onTweetReceived(tweet);
+    // });
   }
+}
+
+async function getFollowerLookup(follower) {
+    T.get('users/lookup', { user_id: follower },  function (err, data, response) {
+      console.log(data);
+      console.log(data['screen_name']);
+    });
 }
 
 async function onTweetReceived(tweet) {
   try {
+    var user_id = tweet['user_id'];
     var lang = tweet['lang'];
     var text = tweet['text'];
     var hashtags = [];
@@ -26,7 +47,7 @@ async function onTweetReceived(tweet) {
       hashtags[i] = tweet['entities']['hashtags'][i]['text'];
     }
 
-    if(hashtags.length > 0) {
+    if(lang === 'fr' && hashtags.length > 0) {
 
       var tweetSchema = new TweetSchema({
         hashtag : hashtags,
@@ -36,7 +57,7 @@ async function onTweetReceived(tweet) {
 
       tweetSchema.save();
 
-      console.log('\x1b[31m', 'Lang: ' + lang);
+      console.log('\x1b[31m', 'Lang: ' + lang + ' User id: ' + user_id);
       console.log('\x1b[32m', 'Hashtags: ' + hashtags);
       console.log('\x1b[34m', 'Text: ' + text + '\x1b[0m\n');
     }
